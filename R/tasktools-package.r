5113f66b9c6c41b4519a580aeea73a4e5d7d720e
Drew Schmidt <wrathematics@gmail.com>
2018-02-12 22:22:14 -0500

    more scaffolding

---
+++ R/tasktools-package.r
@@ -0,0 +1,12 @@
+#' tasktools-package
+#' 
+#' @description
+#' TODO
+#' 
+#' @import pbdMPI
+#' 
+#' @docType package
+#' @name tasktools-package
+#' @author Drew Schmidt
+#' @keywords package
+NULL
